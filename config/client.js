a40fbf60119be76acf7be28ed3ec7b57899c6e1d
George Diab <georgediab@goto10solutions.com>
2018-09-30 22:58:47 -0700

    update callback

--- config/client.js
+++ config/client.js
@@ -1,5 +1,5 @@
 
 module.exports = {
   clientId: '5f2bd34adb0849aaa5c29b3876f07856',
-  redirectUri: 'http://localhost:5000/callback/'
+  redirectUri: 'https://pure-springs-36036.herokuapp.com/callback/'
 }	
